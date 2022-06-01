d01bd0679dc02aedd126458778d70c0916e4fdd7
[alex swiggum] <[ashley.swiggum11@gmail.com]>
2022-06-01 11:46:08 -0500

    updated project3

--- src/mock/data.js
+++ src/mock/data.js
@@ -37,21 +37,21 @@ export const projectsData = [
   },
   {
     id: nanoid(),
-    img: 'project2.png',
-    title: 'Famous Artist [ Database ]',
-    info: 'An Artist Inventory Database where users can add, update, and delete Artists in their user authenticated profile. I utilized Flask Framework, PostgreSQL database and HTML/CSS display.',
-    info2: 'Tech Stack: Flask, PostgreSQL, Heroku',
-    url: 'https://artist-inventory-api.herokuapp.com/',
-    repo: 'https://github.com/swigguma5776/Artist-Database-Api_Flask-Heroku_assignment', // if no repo, the button will not show up
+    img: 'project3.png',
+    title: 'Personal Portfolio',
+    info: 'My personal portfolio is an experiment with the React framework to further grow my skills as a front-end developer. It is an interactive Resume to showcase my skills and learn more about myself and my journey in the tech world.',
+    info2: 'Tech Stack: React, SCSS, Gatsby',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project3.png',
-    title: 'Personal Portfolio',
-    info: '',
-    info2: 'Tech Stack: React, SASS, Gatsby',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'project2.png',
+    title: 'Famous Artist [ Database ]',
+    info: 'An Artist Inventory Database where users can add, update, and delete Artists in their user authenticated profile. I utilized Flask Framework, PostgreSQL database and HTML/CSS display.',
+    info2: 'Tech Stack: Flask, HTML, CSS, PostgreSQL, Heroku',
+    url: 'https://artist-inventory-api.herokuapp.com/',
+    repo: 'https://github.com/swigguma5776/Artist-Database-Api_Flask-Heroku_assignment', // if no repo, the button will not show up
   },
 ];
 
@@ -74,6 +74,11 @@ export const footerData = {
       id: nanoid(),
       name: 'github',
       url: 'https://github.com/swigguma5776',
+    },
+    {
+      id: nanoid(),
+      name: 'interior design portfolio',
+      url: 'https://ashleyswiggum.com',
     },
   ],
 };
